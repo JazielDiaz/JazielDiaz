625a057b2dd67e0043722da30e4bcac8bad6feb7
JazielDiaz <1210234@unav.edu.mx>
2024-11-13 14:27:55 -0700

    Update Router

--- src/js/router.js
+++ src/js/router.js
@@ -1,7 +1,7 @@
 export const router = () => {
     return {
         async home(context) {
-            const response = await fetch(new URL('@/pages/home.html', import.meta.url).href);
+            const response = await fetch(new URL('@/home', import.meta.url).href);
             const htmlContent = await response.text();
             document.querySelector('#main').innerHTML = htmlContent;
         },
