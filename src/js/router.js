2e210adab9ae56ba98b6d37638df0a00a9bbe9f9
JazielDiaz <1210234@unav.edu.mx>
2024-12-10 09:46:57 -0700

    Portfolio 2.0

--- src/js/router.js
+++
@@ -1,9 +0,0 @@
-export const router = () => {
-    return {
-        async home(context) {
-            const response = await fetch(new URL('@/home', import.meta.url).href);
-            const htmlContent = await response.text();
-            document.querySelector('#main').innerHTML = htmlContent;
-        },
-    }
-}
